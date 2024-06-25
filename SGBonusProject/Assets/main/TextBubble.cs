bf2a6741baa735cbb896aa04ac7b898c8ae08f3a
Johannes P <79200769+JP-1999@users.noreply.github.com>
2024-06-25 15:16:01 +0200

    Added Main Game State and Customer Object

---
+++ SGBonusProject/Assets/main/TextBubble.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBubble : MonoBehaviour
+{
+    public Text nameText;
+    public Text instructionsText;
+    public Text orderText;
+    public Text timerText;
+
+    public void SetText(string name, string instructions, string order, float timer)
+    {
+        nameText.text = $"Name: {name}";
+        instructionsText.text = $"Instructions: {instructions}";
+        orderText.text = $"Order: {order}";
+        timerText.text = $"Timer: {timer:F1}";
+    }
+}
